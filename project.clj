813704316722a81e9f01ec3673b949889f69a964
Tim <tim@voodle.de>
2015-03-23 00:09:56 +0100

    initial bfk interpreter

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject brainfuck "1.0.0"
+    :description "Brainfuck interpreter"
+    :dependencies [[org.clojure/clojure "1.6.0"]]
+    :main brainfuck.core)
+
---
+++ src/brainfuck/core.clj
@@ -0,0 +1,51 @@
+(ns brainfuck.core
+  (:gen-class))
+
+; If EOF is read, return 0
+(defn- bf-read-char [] (max 0 (.read System/in)))
+
+(defn bf-find-bracket [prog pos dir]
+  (if (= (nth prog pos) (if (= 1 dir) \] \[))
+    pos
+    (bf-find-bracket prog (+ pos dir) dir)))
+
+(defn bf-set [a j x]
+  (if (< (count a) j)
+    (bf-set (conj a 0) j x)
+    (assoc a j x)))
+
+(defn- bf-step [p state]
+  (let [[i [a j]] state
+        cmd (get p i)
+        v (get a j 0)]
+    (if (= cmd \.) (-> a (get j 0) (max 0) char print))
+    [(inc
+       (case cmd
+         \[ (if (= v 0) (bf-find-bracket p i 1) i)
+         \] (if (not= v 0) (bf-find-bracket p i -1) i)
+         i))
+     (case cmd
+       \< [a (dec j)]
+       \> [a (inc j)]
+       \+ [(bf-set a j (inc v)) j]
+       \- [(bf-set a j (dec v)) j]
+       \, [(bf-set a j (bf-read-char)) j]
+       [a j])]))
+
+(defn- bf-finished [p [i _]]
+  (= (count p) i))
+
+(defn- bf-exec [prog initial-state]
+  (loop [state initial-state]
+    (when-not (bf-finished prog state)
+      (recur (bf-step prog state)))))
+
+(defn bf [prog]
+  (bf-exec prog [0 [[] 0]]))
+
+(defn bf-file [file]
+  (bf (slurp file)))
+
+(defn -main [file & args]
+  (bf-file file))
+
